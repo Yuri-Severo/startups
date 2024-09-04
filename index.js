3be4f090c570742e13b0fc63f0d6f68b3f07c169
Yuri Severo <ryurisevero@gmail.com>
2024-09-04 12:59:19 -0300

    removed athentication system

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const dbConnection = require("./config/dbConnection");
 const startupsRoutes = require("./src/startupsRoutes");
-const authRoutes = require('./src/routes/authRoutes');
 
 require("dotenv").config();
 dbConnection();
@@ -10,7 +9,6 @@ const app = express();
 app.use(express.json());
 
 app.use(startupsRoutes);
-app.use(authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- src/routes/startupsRoutes.js
+++ src/routes/startupsRoutes.js
@@ -3,33 +3,27 @@ const StartupController = require("../controllers/startupController");
 const router = express.Router();
 
 router.get(
-    "/startups/all", 
-    authMiddleware(), 
+    "/startups/all",
     StartupController.getAll
 );
 router.get(
-    "/startup/:id", 
-    authMiddleware(), 
+    "/startup/:id",
     StartupController.getOne
 );
 router.get(
-    "/startups", 
-    authMiddleware(), 
+    "/startups",
     StartupController.getFirst
 );
 router.post(
   "/startups",
-  authMiddleware("admin"),
   StartupController.registerOne
 );
 router.put(
   "/startups/:id",
-  authMiddleware("admin"),
   StartupController.updateOne
 );
 router.delete(
   "/startups/:id",
-  authMiddleware("admin"),
   StartupController.deleteOne
 );
 
